d9e0fb05db43ce801ec81315c230c7fa1ebb51ad
Guillermo <aptoguilermo@gmail.com>
2025-12-29 12:44:18 +0100

    Protect CRM routes with auth guard

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,35 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LibroVentas from "./pages/LibroVentas";
 import NuevaVenta from "./pages/ventas/NuevaVenta";
 import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* LOGIN */}
         <Route path="/login" element={<Login />} />
-        <Route path="/crm/libro-ventas" element={<LibroVentas />} />
-        <Route path="/crm/nueva-venta" element={<NuevaVenta />} />
+
+        {/* RUTAS PROTEGIDAS */}
+        <Route
+          path="/crm/libro-ventas"
+          element={
+            <ProtectedRoute>
+              <LibroVentas />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/crm/nueva-venta"
+          element={
+            <ProtectedRoute>
+              <NuevaVenta />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* DEFAULT */}
         <Route path="*" element={<Navigate to="/crm/libro-ventas" />} />
       </Routes>
     </BrowserRouter>
